e93eae9e12a820bb77cea3f92b0bb298ff054eb9
andrea <andrea@andrea-Lenovo-Y50-70>
2019-10-16 14:36:51 +0200

    Changeson packages

---
+++ Demo Sprint 1/src/gui/DemoInterface.java
@@ -0,0 +1,12 @@
+package gui;
+
+
+public interface DemoInterface {
+
+	// Method called when a button is pressed
+	public void askTicket();
+	
+	// Method used to print the ticket on a textArea
+	public void printTicket();
+	
+}
