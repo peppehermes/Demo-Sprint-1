7ac520f0516944d9b023bc38b1357728311ada84
peppehermes <giuse@192.168.1.9>
2019-10-16 14:28:25 +0200

    Added class Ticket, removed class Prova

---
+++ Demo Sprint 1/src/Ticket.java
@@ -0,0 +1,33 @@
+import java.util.Date;
+
+public class Ticket {
+	private char label;
+	private int number;
+	private Date d;
+	
+	public void Ticket() { }
+
+	public char getLabel() {
+		return label;
+	}
+
+	public void setLabel(char label) {
+		this.label = label;
+	}
+
+	public int getNumber() {
+		return number;
+	}
+
+	public void setNumber(int number) {
+		this.number = number;
+	}
+
+	public Date getDate() {
+		return d;
+	}
+
+	public void setDate(Date d) {
+		this.d = d;
+	}
+}
